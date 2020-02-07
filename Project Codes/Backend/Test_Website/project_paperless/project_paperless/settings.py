7657067a481408abf9e22e5338933410b2cac13d
TanzimAhmed <47281426+TanzimAhmed@users.noreply.github.com>
2020-02-08 00:49:54 +0600

    Database settings update
    Added settings for both SQLite3 and MySQL.

--- Project Codes/Backend/Test_Website/project_paperless/project_paperless/settings.py
+++ Project Codes/Backend/Test_Website/project_paperless/project_paperless/settings.py
@@ -74,11 +74,23 @@ WSGI_APPLICATION = 'project_paperless.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+# Databases list
+database = {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_paperless',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
+}
+
+DATABASES = {
+    'default': database['sqlite3']
 }
 
 
