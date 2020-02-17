80878107047a68d53a96d185e33af96d0a1f57ae
TanzimAhmed <tanzim.din@northsouth.edu>
2020-02-17 11:23:14 +0600

    Updated Users Model.
    - Users model fields can be customized now.

--- Project Codes/Backend/Test_Website/source/project_paperless/settings.py
+++ Project Codes/Backend/Test_Website/source/project_paperless/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'creative_content'
 ]
 
@@ -97,6 +98,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'users.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
